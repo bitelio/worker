2138e720c530456c6d99d660add383f25ebd6aa0
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2017-09-08 13:43:35 +0200

    Added SendGrid alerts

--- worker/alerts.py
+++ worker/alerts.py
@@ -1,23 +1,32 @@
-import sendgrid
+from logging import getLogger
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers import mail
 
 from . import config
 
 
 class SendGrid:
     def __init__(self):
-        self.sg = sendgrid.SendGridAPIClient(apikey=config.SENDGRID['APIKEY'])
+        self.log = getLogger(self.__class__.__qualname__)
+        self.sg = SendGridAPIClient(apikey=config.SENDGRID['APIKEY'])
         self.sender = config.SENDGRID['SENDER']
         self.receiver = config.SENDGRID['RECEIVER']
 
     def send(self, error, traceback):
-        if all((self.client, self.sender, self.receiver)):
-            sender = sendgrid.helpers.mail.Email(self.sender)
-            to = sendgrid.helpers.mail.Email(self.receiver)
+        if all((self.sg, self.sender, self.receiver)):
+            sender = mail.Email(self.sender)
+            to = mail.Email(self.receiver)
             subject = f'Worker: {error}'
-            content = Content("text/plain", traceback)
-            mail = sendgrid.helpers.mail.Mail(sender, subject, to, content)
-            response = self.sg.client.mail.send.post(request_body=mail.get())
+            content = mail.Content("text/plain", traceback.format_exc())
+            email = mail.Mail(sender, subject, to, content)
+            response = self.post(email.get())
+            print(self.receiver)
+            print(self.sender)
             if response.status_code == 200:
-                log.info(f'Sent email alert to {receiver}')
+                self.log.info(f'Sent email alert to {self.receiver}')
             else:
-                log.error(f'Error sending email: {response.status_code}')
+                self.log.error(f'Error sending email: {response.status_code}')
+
+    def post(self, body):
+        return self.sg.client.mail.send.post(request_body=body)
+
