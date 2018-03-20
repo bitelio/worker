15981055936108df9064b58036f978975e4ff477
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2018-03-20 18:35:35 +0100

    Added rotating handler

--- worker/env/live.py
+++ worker/env/live.py
@@ -3,6 +3,7 @@ from sys import stdout
 from redis import StrictRedis
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
+from logging.handlers import RotatingFileHandler
 from raven.handlers.logging import SentryHandler
 from raven.conf import setup_logging
 from pythonjsonlogger.jsonlogger import JsonFormatter
@@ -16,14 +17,18 @@ env.cache = StrictRedis("redis")
 env.mongo = MongoClient(getenv("MONGODB"), connectTimeoutMS=30000,
                         socketTimeoutMS=None, socketKeepAlive=True)
 env.db = env.mongo.bitelio
-env.log.setLevel("INFO")
+rotating_handler = RotatingFileHandler("/var/log/worker.log", "a", 5**10, 2)
+env.log.addHandler(rotating_handler)
 sentry_handler = SentryHandler(getenv("SENTRY"))
 sentry_handler.setLevel("ERROR")
 setup_logging(sentry_handler)
 env.processors.append(JSONRenderer())
 env.configure(processors=env.processors)
+env.log.setLevel("INFO")
 
 leankit = getLogger("leankit")
-handler = StreamHandler(stdout)
-handler.setFormatter(JsonFormatter())
-leankit.addHandler(handler)
+stream_handler = StreamHandler(stdout)
+stream_handler.setFormatter(JsonFormatter())
+leankit.addHandler(stream_handler)
+leankit.addHandler(rotating_handler)
+leankit.setLevel("WARNING")
